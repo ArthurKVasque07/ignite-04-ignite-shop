07c36a460f8c02467458e55806db6bd8392a913e
Arthur Kavagouth Vasque <arthurkvasque.eng@outlook.com>
2023-01-23 15:17:18 -0300

    global styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,7 @@
+import { globalStyles } from "@/styles/global";
 import { AppProps } from "next/app";
+
+globalStyles();
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
